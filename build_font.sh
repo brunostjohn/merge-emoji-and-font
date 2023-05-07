80a8c47367993dcc8cf4df4c0a87247c23a7a90f
Bruno St. John <brunost.john@icloud.com>
2023-05-07 19:35:07 +0100

    fix(bug): creating the font takes ages because of google's nanoemoji and usually crashes by the end. let's do it concurrently

--- build_font.sh
+++ build_font.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
 node ./convert_to_svg.js
-mv ./build/svg ./nanoemoji
-cd nanoemoji
-python3 -m venv ./
-source bin/activate
-python3 -m pip install -e .
-nanoemoji --ignore_reuse_error -v 0 --color_format=glyf_colr_0 $(find ./svg -name '*.svg')
+
+for f in ./build
+do
+    mkdir ./nanoemoji-$f
+    cp -R ./nanoemoji/* ./nanoemoji-$f/
+    mv ./build/$f ./nanoemoji-$f/svg
+    python3 -m venv ./nanoemoji-$f/
+    cd ./nanoemoji-$f
+    source bin/activate
+    python3 -m pip install -e .
+    deactivate
+done
+
+for f in ./build
+do
+    (cd ./nanoemoji-$f/ && source bin/activate && nanoemoji --ignore_reuse_error -v 0 --color_format=glyf_colr_0 $(find ./svg -name '*.svg') && deactivate) &
+done
+
+wait
--- convert_to_svg.js
+++ convert_to_svg.js
@@ -2,7 +2,44 @@ const fs = require("fs");
 const path = require("path");
 const child_process = require("child_process");
 
-function create_emoji_svg(file) {
+if (fs.existsSync(path.join(__dirname, "build"))) {
+  fs.rmSync(path.join(__dirname, "build"), {
+    recursive: true,
+    force: true,
+  });
+}
+
+const svgs = fs.readdirSync(
+  path.join(__dirname, "apple-emoji-linux", "png", "160")
+);
+
+const range = Math.ceil(svgs.length / 500);
+
+fs.mkdirSync(path.join(__dirname, "build"));
+
+for (let i = 0; i < range; i++) {
+  fs.mkdirSync(path.join(__dirname, "build", "svg" + (i + 1)));
+}
+
+const folderLimit = Math.ceil(svgs.length / range);
+
+let doneCounter = 0;
+let currentFolder = 1;
+let doneCounterForFolder = 0;
+
+svgs.forEach((file) => {
+  if (doneCounterForFolder >= folderLimit) {
+    currentFolder++;
+    doneCounterForFolder = 0;
+  }
+
+  process.stdout.clearLine();
+  process.stdout.cursorTo(0);
+
+  process.stdout.write(
+    `[${doneCounter}/${svgs.length}] ${file}                  `
+  );
+
   child_process.execSync(
     `vtracer --input ${path.join(
       __dirname,
@@ -13,23 +50,10 @@ function create_emoji_svg(file) {
     )} --output ${path.join(
       __dirname,
       "build",
-      "svg",
+      "svg" + currentFolder,
       file.slice(0, -4) + ".svg"
-    )} -p 8 --colormode color -m pixel`,
-    { stdio: "inherit" }
+    )} -p 8 --colormode color -m pixel`
   );
-}
-
-if (fs.existsSync(path.join(__dirname, "build"))) {
-  fs.rmSync(path.join(__dirname, "build"), {
-    recursive: true,
-    force: true,
-  });
-}
-
-fs.mkdirSync(path.join(__dirname, "build"));
-fs.mkdirSync(path.join(__dirname, "build", "svg"));
-
-fs.readdirSync(path.join(__dirname, "apple-emoji-linux", "png", "160")).forEach(
-  (file) => create_emoji_svg(file)
-);
+  doneCounter++;
+  doneCounterForFolder++;
+});
